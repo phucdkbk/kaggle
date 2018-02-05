d32c4159bf076511e30da3ff678a6f748b3bb69a
phuc <phucdkbk@gmail.com>
2018-02-05 18:38:56 +0700

    format code

---
+++ ToxicCommentClassification/SourceCode/TCC/TestPy.py
@@ -0,0 +1,17 @@
+import numpy as np
+myList=[1,2,3,4,5,6]
+
+print(myList[2])
+myList[2:5]
+print(myList[2:5])
+print(myList[5:])
+
+array = [[1,2,3,4,5],[6,7,8,9,10]]
+
+mat = np.array(array)
+
+tup1 = ('physics', 'chemistry', 1997, 2000);
+
+print(mat[:, 2:3])
+
+# [:, 2:]
